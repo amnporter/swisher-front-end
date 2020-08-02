e79c3fbcfe625e8206d32de92d6a5e3dff9a58c8
Porter, Chris <amnporter@gmail.com>
2020-08-02 15:18:08 -0400

    add local/prod envs

--- src/app/services/data/data.service.ts
+++ src/app/services/data/data.service.ts
@@ -3,6 +3,7 @@ import { Router } from '@angular/router';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { ReplaySubject, Observable, of } from 'rxjs';
 import { take, map, catchError } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
 
 export enum ItemGroup {
   CIGARS_CIGARELLOS = 'Cigars & Cigarellos',
@@ -21,7 +22,7 @@ export interface ListItem {
   price: number;
 }
 
-const API_URL = 'http://localhost:3000/items';
+const API_URL = environment.baseUrl;
 @Injectable({
   providedIn: 'root'
 })
@@ -32,17 +33,17 @@ export class DataService {
   constructor(private http: HttpClient, private router: Router) { }
 
   public getList(): Observable<ListItem[]> {
-    return this.http.get<ListItem[]>(`${API_URL}`, this.getHttpOptions());
+    return this.http.get<ListItem[]>(`${API_URL}/items`, this.getHttpOptions());
   }
 
   public getListItem(id: number): Observable<ListItem> {
-    return this.http.get<ListItem>(`${API_URL}/${id}`, this.getHttpOptions());
+    return this.http.get<ListItem>(`${API_URL}/items/${id}`, this.getHttpOptions());
   }
 
   public updateItemInventory(quantity: number, item: ListItem): void {
     const updatedInventory = item.inventory + quantity;
     item.inventory = updatedInventory;
-    this.http.put(`${API_URL}/${item.id}`, item, this.getHttpOptions()).pipe(take(1)).subscribe(data => {
+    this.http.put(`${API_URL}/items/${item.id}`, item, this.getHttpOptions()).pipe(take(1)).subscribe(data => {
       this.inventoryUpdated$.next();
     }, err => {
       throw new Error(err);
@@ -54,7 +55,7 @@ export class DataService {
   }
 
   public replenishInventory(): void {
-    this.http.get(`${API_URL}?replenish=true`, this.getHttpOptions()).pipe(take(1)).subscribe(data => {
+    this.http.get(`${API_URL}/items?replenish=true`, this.getHttpOptions()).pipe(take(1)).subscribe(data => {
       this.productList = data as ListItem[];
       this.inventoryUpdated$.next(true);
       this.router.navigate(['']);
